5dacd13021e13ec69aa1347733a8543e35cb14b6
nahook1 <nahook@qq.com>
2019-10-16 23:12:44 +0800

    x

--- ch.sh
+++ ch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-git filter-branch -f --env-filter '
+git filter-branch --env-filter '
 
-OLD_EMAIL="nahook@bccto.me"
+OLD_NAME="haha"
+OLD_EMAIL="your-old-email@example.com"
 CORRECT_NAME="nahook1"
 CORRECT_EMAIL="nahook@qq.com"
 
@@ -11,7 +12,18 @@ then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
